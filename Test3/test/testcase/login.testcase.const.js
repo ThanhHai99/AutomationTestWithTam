b63fffc0e2c403c555758475c902b7a36b15ab6e
HaiTran <hai.tran@kyanon.digital>
2022-08-21 22:26:08 +0700

    update

--- Test3/test/testcase/login.testcase.const.js
+++ Test3/test/testcase/login.testcase.const.js
@@ -1,14 +1,3 @@
-// export const LoginTestCase = {
-//   TestCase1: {
-//     username: "u1",
-//     password: "p1",
-//   },
-//   TeseCase2: {
-//     username: "tomsmith",
-//     password: "SuperSecretPassword!",
-//   },
-// };
-
 module.exports = {
   TestCase1: {
     username: "u1",
