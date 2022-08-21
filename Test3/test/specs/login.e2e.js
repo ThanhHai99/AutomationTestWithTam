d57b3354bfa16b04656e291920e5a653d8971447
HaiTran <hai.tran@kyanon.digital>
2022-08-21 22:04:26 +0700

    update

--- Test3/test/specs/login.e2e.js
+++ Test3/test/specs/login.e2e.js
@@ -1,10 +1,19 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
+const loginTestcaseConst = require('../testcase/login.testcase.const');
+// const LoginTestCase = require('../testcase/login.testcase.const')
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+    it('should not login with valid credentials', async () => { // expect fail
         await LoginPage.open();
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await LoginPage.login(loginTestcaseConst.TestCase1.username, loginTestcaseConst.TestCase1.password);
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!');
+    });
+
+    it('should login with valid credentials', async () => { // expect success
+        await LoginPage.open();
+        await LoginPage.login(loginTestcaseConst.TeseCase2.username, loginTestcaseConst.TeseCase2.password);
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!');
     });
---
+++ Test3/test/testcase/login.testcase.const.js
@@ -0,0 +1,21 @@
+// export const LoginTestCase = {
+//   TestCase1: {
+//     username: "u1",
+//     password: "p1",
+//   },
+//   TeseCase2: {
+//     username: "tomsmith",
+//     password: "SuperSecretPassword!",
+//   },
+// };
+
+module.exports = {
+  TestCase1: {
+    username: "u1",
+    password: "p1",
+  },
+  TeseCase2: {
+    username: "tomsmith",
+    password: "SuperSecretPassword!",
+  },
+}
